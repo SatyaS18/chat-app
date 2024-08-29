3ee86879fe15f961b9a662dd66889458cc531b0f
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-29 06:36:30 +0530

    add chat sidebar

--- src/components/chats-layout.tsx
+++ src/components/chats-layout.tsx
@@ -2,11 +2,16 @@
 
 import { FC, ReactNode } from "react";
 import SharedLayout from "./shared-layout";
+import ChatSidebar from "./chat-sidebar";
 
 type ChatsLayoutProps = {
   children: ReactNode;
 };
 
 export const ChatsLayout: FC<ChatsLayoutProps> = ({ children }) => {
-  return <SharedLayout SidebarComponent={() => <></>}>{children}</SharedLayout>;
+  return (
+    <SharedLayout SidebarComponent={() => <ChatSidebar />}>
+      {children}
+    </SharedLayout>
+  );
 };

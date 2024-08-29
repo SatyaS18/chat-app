24d166f798c4c4f5262bbd0d441582319a055d85
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-29 16:12:08 +0530

    add input component

--- src/components/profile-dialog-content.tsx
+++ src/components/profile-dialog-content.tsx
@@ -2,6 +2,7 @@ import { useTheme } from "next-themes";
 import { Card, CardTitle } from "./ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { UserRound } from "lucide-react";
+import { Input } from "./ui/input";
 
 const statuses = [
   "👋 Speak Freely",
@@ -19,7 +20,7 @@ const ProfileDialogContent = () => {
         <CardTitle>Profile</CardTitle>
 
         <div>
-          <Avatar>
+          <Avatar className="h-20 w-20 mx-auto">
             <AvatarImage src="https://github.com/shadcn.png" />
             <AvatarFallback>User</AvatarFallback>
           </Avatar>
@@ -29,6 +30,12 @@ const ProfileDialogContent = () => {
       <div className="flex flex-col gap-y-6 mt-4">
         <div className="flex items-center space-x-2">
           <UserRound />
+          <Input
+            disabled
+            placeholder="Name"
+            className="border-none outline-none ring-0"
+            value="Satyakant Sahu"
+          />
         </div>
       </div>
     </div>

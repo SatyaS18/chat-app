03f446f22efe6a7d5b0061e0d79217c599f897ed
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-08-29 16:08:44 +0530

    add avatar

--- src/components/profile-dialog-content.tsx
+++ src/components/profile-dialog-content.tsx
@@ -1,4 +1,7 @@
 import { useTheme } from "next-themes";
+import { Card, CardTitle } from "./ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { UserRound } from "lucide-react";
 
 const statuses = [
   "👋 Speak Freely",
@@ -10,7 +13,26 @@ const statuses = [
 
 const ProfileDialogContent = () => {
   const { setTheme } = useTheme();
-  return <div className="">Profile-dialog-content</div>;
+  return (
+    <div>
+      <Card className="border-0 flex flex-col space-y-4">
+        <CardTitle>Profile</CardTitle>
+
+        <div>
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>User</AvatarFallback>
+          </Avatar>
+        </div>
+      </Card>
+
+      <div className="flex flex-col gap-y-6 mt-4">
+        <div className="flex items-center space-x-2">
+          <UserRound />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileDialogContent;

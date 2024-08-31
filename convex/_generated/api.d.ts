fdbfdcef9ac8a018419572e94fc8e0acef4c27f9
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-09-01 01:21:45 +0530

    add getUserDataById function

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,6 +16,7 @@ import type {
 import type * as http from "../http.js";
 import type * as status from "../status.js";
 import type * as user from "../user.js";
+import type * as _utils from "../_utils.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -29,6 +30,7 @@ declare const fullApi: ApiFromModules<{
   http: typeof http;
   status: typeof status;
   user: typeof user;
+  _utils: typeof _utils;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,

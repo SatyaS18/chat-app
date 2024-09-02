2636913c5f3ed8ed8bf9fee996748ca52d371c4c
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-09-03 00:16:56 +0530

    add getLastMessageDetails function

--- convex/conversations.ts
+++ convex/conversations.ts
@@ -1,3 +1,38 @@
+import { Id } from "./_generated/dataModel";
+import { MutationCtx, QueryCtx } from "./_generated/server";
+
+const getLastMessageDetails = async ({
+  ctx,
+  conversationId,
+}: {
+  ctx: QueryCtx | MutationCtx;
+  conversationId: Id<"messages"> | undefined;
+}) => {
+  if (!conversationId) {
+    return null;
+  }
+
+  const message = await ctx.db.get(conversationId);
+
+  if (!message) {
+    return null;
+  }
+
+  const sender = await ctx.db.get(message.senderId);
+
+  if (!sender) {
+    return null;
+  }
+
+  const content = getMessageContent(message.type, message.content);
+
+  return {
+    lastMessageSender: sender.username,
+    lastMessageContent: content,
+    lastMessageTimestamp: message._creationTime,
+  };
+};
+
 const getMessageContent = (type: string, content: any) => {
   switch (type) {
     case "text":

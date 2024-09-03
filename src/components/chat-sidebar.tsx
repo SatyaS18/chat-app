b8ea2811234bd82d28a53160f0223aa5fc816377
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-09-03 10:56:10 +0530

    add chatlist

--- src/components/chat-sidebar.tsx
+++ src/components/chat-sidebar.tsx
@@ -2,13 +2,14 @@
 // import { ChatList } from "@/components/chat-list";
 // import { NewGroup } from "@/components/new-group";
 
+import ChatList from "./chat-list";
 import { SidebarContainer } from "./sidebar-container";
 
 const ChatSidebar = () => {
   return (
     <>
       <SidebarContainer title="Chats" trigger={<></>}>
-        <div>Chat Sidebar</div>
+        <ChatList />
       </SidebarContainer>
     </>
   );

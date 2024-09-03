e05bdb3ae23137a7bf80c55f77e2dead7ce33b3f
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-09-03 19:50:43 +0530

    add logo and content

--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+import NewGroup from "@/components/new-group";
 import { NavigationBar } from "./../../components/navigation-bar";
+import { FaSignalMessenger } from "react-icons/fa6";
 
 const ChatPage = () => {
   return (
-    <div className="">
-      <NavigationBar trigger={<></>} />
-    </div>
+    <>
+      <NavigationBar trigger={<NewGroup />} />
+      <div className="hidden md:grid h-full max-w-56 text-center mx-auto place-content-center">
+        <FaSignalMessenger className="mx-auto text-primary-main" size={200} />
+        <p className="text-sm mt-5 text-primary-main">
+          Welcome to the chat app! Start a new chat or select an existing one to
+          get started.
+        </p>
+      </div>
+      <div className="md:hidden flex flex-col space-y-2"></div>
+    </>
   );
 };
 

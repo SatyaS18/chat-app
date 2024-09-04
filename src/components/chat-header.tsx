efa7675f0e5ece802906106b5c9a55c329ab9cc9
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-09-04 10:48:49 +0530

    initialize functions

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -1,4 +1,35 @@
-const ChatHeader = () => {
+import { useIsDesktop } from "@/hooks/use-is-desktop";
+import { useSidebarWidth } from "@/hooks/use-sidebar-width";
+import { useQuery } from "convex/react";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
+import { api } from "../../convex/_generated/api";
+
+type ChatHeaderProps = {
+  chatAvatar: string;
+  username: string;
+  isGroup: boolean;
+  chatId: string;
+  status: string;
+};
+
+export const ChatHeader: FC<ChatHeaderProps> = ({
+  chatAvatar,
+  chatId,
+  isGroup,
+  status,
+  username,
+}) => {
+  const { sidebarWidth } = useSidebarWidth();
+  const isDesktop = useIsDesktop();
+  const router = useRouter();
+  const conversations = useQuery(api.conversations.get);
+
+  const groupsInCommon = conversations?.filter(
+    ({ conversation }) => conversation.isGroup
+  );
+
+  const videoCall = () => router.push(`/calls/${chatId}`);
   return <div className="">ChatHeader</div>;
 };
 

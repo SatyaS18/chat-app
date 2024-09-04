03f69cd40d993cacfd55e8a985aadd343e371653
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-09-04 16:05:31 +0530

    add action button

--- src/components/profile-sheet.tsx
+++ src/components/profile-sheet.tsx
@@ -1,5 +1,17 @@
 import { FC } from "react";
 
+type ActionButtonProps = {
+  Icon: FC;
+  label: string;
+};
+
+const ActionButton: FC<ActionButtonProps> = ({ Icon, label }) => (
+  <div className="flex space-y-2 flex-col items-center w-fit px-4 py-2 rounded-xl bg-gray-100 dark:bg-gray-800">
+    <Icon />
+    <p className="text-xs">{label}</p>
+  </div>
+);
+
 type ProfileSheetProps = {
   chatId: string;
   username: string;
